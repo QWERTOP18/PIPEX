80127f34441b8f4b47ab758794dfe283702d3167
Yui Mizukami <ymizukam@c4r3s17.42tokyo.jp>
2024-12-05 03:51:48 +0900

    pid

--- main.c
+++ main.c
@@ -6,13 +6,14 @@
 /*   By: ymizukam <ymizukam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 00:25:08 by ymizukam          #+#    #+#             */
-/*   Updated: 2024/12/05 02:54:54 by ymizukam         ###   ########.fr       */
+/*   Updated: 2024/12/05 03:51:33 by ymizukam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #define PATH 5
 #include "libft.h"
@@ -27,7 +28,7 @@ char	**fetch_absolute_path(char **env)
 
 	while (*env)
 	{
-		if (strncmp(*env, "PATH=", PATH) == 0)
+		if (ft_strncmp(*env, "PATH=", PATH) == 0)
 		{
 			path = *env + PATH;
 			return (ft_split(path, ':'));
@@ -39,8 +40,9 @@ char	**fetch_absolute_path(char **env)
 
 int	main(int argc, char **argv, char **env)
 {
-	int fd1;
-	int fd2;
+	int fd_from;
+	int fd_via;
+	int fd_to;
 	/*
 	./pipex file1 cmd1 cmd2 file2
 	*/
@@ -52,13 +54,26 @@ int	main(int argc, char **argv, char **env)
 	// 	return (1);
 	// }
 	char **absolute_path = fetch_absolute_path(env);
-	printf("main done");
-
+	char *av[] = {"ls", "-l", NULL};
+	pid_t pid;
+	pid = fork();
+	if (pid == 0)
+	{
+		printf("This is the child process!\n");
+		printf("%d\n", execve("/bin/ls", av, env));
+		perror("execv:");
+	}
+	else
+	{
+		// This block is executed by the parent process
+		printf("This is the parent process. Child PID is: %d\n", pid);
+		wait(NULL); // Parent waits for the child to finish
+	}
 	printf("%s\n", absolute_path[0]);
 	// to do make wrapper func of open
-	// fd1 = open(argv[1], O_RDWR);
-	// fd2 = open(argv[argc - 1], O_CREAT | O_WRONLY | O_TRUNC, 0644);
-	// if (fd1 == -1 || fd2 == -1)
+	// fd_from = open(argv[1], O_RDWR);
+	// fd_to = open(argv[argc - 1], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	// if (fd_from == -1 || fd_to == -1)
 	// {
 	// 	perror("Error while opening files");
 	// 	return (1);
