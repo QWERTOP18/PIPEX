e1c2d8f6abbf5eea6d0b64ad141906d2178716ce
QWERTOP18 <mz18yui@gmail.com>
2024-12-22 13:14:50 +0900

    merge

--- inc/system.h
+++ inc/system.h
@@ -16,32 +16,51 @@ typedef enum e_status
 
 } t_status;
 
+// typedef struct s_temp
+// {
+//     char **argv;
+//     int pipefds[2];
+//     int openfds[2];
+// } t_temp;
+
 typedef struct s_info
 {
     t_status status;
     t_btree *root;
     char **env;
     char **env_path;
-    void (*__exit)(void *, int);
+    int pipefds[2];
+    //t_temp temp;
+    //void (*__exit)(void *, int);
 }t_info;
 
 
-
+#define REDIRECT 4
+#define OUT 1
 
 typedef struct s_node {
     // t_type type;
     enum 
     {
         TOKEN_CMD,
+        TOKEN_BUILDIN,
         TOKEN_PIPE,
         TOKEN_EOF,
-        // TOKEN_ARGS,
-        TOKEN_REDIRECT,
+        //TOKEN_ARGS,
+        //TOKEN_REDIRECT,
+        TOKEN_REDIR_IN = 0b10000,
+        TOKEN_HEREDOC = 0b10001,
+        TOKEN_REDIR_OUT = 0b10010,
+        TOKEN_REDIR_APPEND = 0b10011,
     } e_type;
     union {
-        char *args;
+        char **argv;
+        char *file;
         int pipefds[2];
     } u_val;
 } t_node;
 
+
+void system_exit(t_info *info,int status);
+t_info *system_init(char **env);
 #endif
--- inc/xunistd.h
+++ inc/xunistd.h
@@ -1,10 +1,15 @@
 #ifndef XUNISTD_H
 #define XUNISTD_H
 #include <unistd.h>
-
+#include "system.h"
 
 void xfree(void **ptr);
 int xclose(int *fd);
+void xpipe(int pipfds[], t_info *info);
+
+pid_t xfork(t_info *info);
+
+void *xmalloc(size_t size,t_info *info);
 
 
 #endif
